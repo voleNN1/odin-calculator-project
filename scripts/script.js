fed4dd1e6573b8f9898741e2e304fd3c51e29a57
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-25 00:33:26 -0500

    Define variables for buttons, begin adding events to buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-let inputOne = 0;
-let inputTwo = 0;
+let inputOne = "";
+let inputTwo = "";
 let inputOperator = "";
 
 function addNum(numOne, numTwo) {
@@ -37,8 +37,61 @@ function operate(numOne, oper, numTwo) {
     }
 }
 
-function populateDisplay() {
-    
-}
+const displayOne = document.querySelector("#num-one");
+const displayOper = document.querySelector("#operator");
+const displayTwo = document.querySelector("#num-two");
+const displayTotal = document.querySelector("#result");
+const btnZero = document.querySelector("#zero");
+const btnOne = document.querySelector("#one");
+const btnTwo = document.querySelector("#two");
+const btnThree = document.querySelector("#three");
+const btnFour = document.querySelector("#four");
+const btnFive = document.querySelector("#five");
+const btnSix = document.querySelector("#six");
+const btnSeven = document.querySelector("#seven");
+const btnEight = document.querySelector("#eight");
+const btnNine= document.querySelector("#nine");
+const btnAdd = document.querySelector("#add");
+const btnSubtract = document.querySelector("#subtract");
+const btnMultiply = document.querySelector("#multiply");
+const btnDivide = document.querySelector("#divide");
+const btnClear = document.querySelector("clear");
+const btnEquals = document.querySelector("equals");
+const btnDecimal = document.querySelector("decimal");
+
+btnZero.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOne.textContent += "0";
+        inputOne += "0";
+    }
+    else {
+        displayTwo.textContent += "0";
+        inputTwo += "0";
+    }  
+})
+
+btnOne.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOne.textContent += "1";
+        inputOne += "1";
+    }
+    else {
+        displayTwo.textContent += "1";
+        inputTwo += "1";
+    }
+})
+
+btnTwo.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOne.textContent += "2";
+        inputOne += "2";
+    }
+    else {
+        displayTwo.textContent += "2";
+        inputTwo += "2";
+    }
+})
+
+
 
 console.log(operate(8, '-', 8));
