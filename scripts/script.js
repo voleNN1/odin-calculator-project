6b1c38fd4b273e38f605db0256ba79056450258c
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-24 21:31:01 -0500

    Create basic operator functions

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function addNum(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtractNum(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiplyNum(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divideNum(numOne, numTwo) {
+    return numOne / numTwo;
+}
