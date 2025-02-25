3e1c65935baa7648f7ccfc2107deae538a27e463
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-24 21:34:06 -0500

    Test operator functions, declare input variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+let inputOne = 0;
+let inputTwo = 0;
+let inputOperator = "";
+
 function addNum(numOne, numTwo) {
     return numOne + numTwo;
 }
@@ -13,3 +17,9 @@ function multiplyNum(numOne, numTwo) {
 function divideNum(numOne, numTwo) {
     return numOne / numTwo;
 }
+
+
+console.log(addNum(5, 5));
+console.log(subtractNum(10, 5));
+console.log(multiplyNum(5, 10));
+console.log(divideNum(25, 5));
