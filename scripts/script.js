3b5b98b46892bdeb5ab4e2893cf5a9ae517cfb77
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-25 00:48:10 -0500

    Add logic to number button event listeners, establish logic for operator buttons

--- scripts/script.js
+++ scripts/script.js
@@ -68,7 +68,7 @@ btnZero.addEventListener("click", () => {
         displayTwo.textContent += "0";
         inputTwo += "0";
     }  
-})
+});
 
 btnOne.addEventListener("click", () => {
     if (inputOperator === "") {
@@ -79,7 +79,7 @@ btnOne.addEventListener("click", () => {
         displayTwo.textContent += "1";
         inputTwo += "1";
     }
-})
+});
 
 btnTwo.addEventListener("click", () => {
     if (inputOperator === "") {
@@ -90,22 +90,119 @@ btnTwo.addEventListener("click", () => {
         displayTwo.textContent += "2";
         inputTwo += "2";
     }
-})
+});
 
 btnThree.addEventListener("click", () => {
-
-})
+    if (inputOperator === "") {
+        displayOne.textContent += "3";
+        inputOne += "3";
+    }
+    else {
+        displayTwo.textContent += "3";
+        inputTwo += "3";
+    }
+});
 
 btnFour.addEventListener("click", () => {
-
-})
+    if (inputOperator === "") {
+        displayOne.textContent += "4";
+        inputOne += "4";
+    }
+    else {
+        displayTwo.textContent += "4";
+        inputTwo += "4";
+    }
+});
 
 btnFive.addEventListener("click", () => {
-
-})
+    if (inputOperator === "") {
+        displayOne.textContent += "5";
+        inputOne += "5";
+    }
+    else {
+        displayTwo.textContent += "5";
+        inputTwo += "5";
+    }
+});
 
 btnSix.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOne.textContent += "6";
+        inputOne += "6";
+    }
+    else {
+        displayTwo.textContent += "6";
+        inputTwo += "6";
+    }
+});
 
-})
+btnSeven.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOne.textContent += "7";
+        inputOne += "7";
+    }
+    else {
+        displayTwo.textContent += "7";
+        inputTwo += "7";
+    }
+});
 
-console.log(operate(8, '-', 8));
+btnEight.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOne.textContent += "8";
+        inputOne += "8";
+    }
+    else {
+        displayTwo.textContent += "8";
+        inputTwo += "8";
+    }
+});
+
+btnNine.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOne.textContent += "9";
+        inputOne += "9";
+    }
+    else {
+        displayTwo.textContent += "9";
+        inputTwo += "9";
+    }
+});
+
+btnAdd.addEventListener("click", () => {
+    if (inputOperator === "") {
+        displayOper.textContent = "+";
+        inputOperator = "+";
+    }
+    else if (inputOperator === "+") {
+        alert("Operator already selected!");
+    }
+    else {
+        displayOper.textContent = "+";
+        inputOperator = "+";
+    }
+});
+
+btnSubtract.addEventListener("click", () => {
+
+});
+
+btnMultiply.addEventListener("click", () => {
+
+});
+
+btnDivide.addEventListener("click", () => {
+
+});
+
+btnEquals.addEventListener("click", () => {
+
+});
+
+btnClear.addEventListener("click", () => {
+
+});
+
+btnDecimal.addEventListener("click", () => {
+
+});
